34464d8225c87da553c2c98add87ea93f8bb6a76
Lucas Paz <47407604+lucaspaz89@users.noreply.github.com>
2022-11-23 12:25:19 -0300

    Programs
    Se Corrige la conexión entre el back con el front

--- Program.cs
+++ Program.cs
@@ -6,83 +6,216 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 
+//var builder = WebApplication.CreateBuilder(args);
+//{
+//    // Add services to the container.
+
+//    //----------------------------------------------CONFIGURACION JWT START---------------------------------------------//
+
+//    builder.Configuration.AddJsonFile("appsettings.json");
+//    var secretKey = builder.Configuration.GetSection("settings").GetSection("secretkey").ToString();
+//    var keyBytes = Encoding.UTF8.GetBytes(secretKey);
+
+//    builder.Services.AddAuthentication(config =>
+//    {
+//        config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//        config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+//    }).AddJwtBearer(config =>
+//    {
+//        config.RequireHttpsMetadata = true;
+//        config.SaveToken = true;
+//        config.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuerSigningKey = true,
+//            IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+//            ValidateIssuer = false,
+//            ValidateAudience = false
+//        };
+//    });
+
+//    //----------------------------------------------CONFIGURACION JWT END---------------------------------------------//
+
+//    //--------------------------------------------CONFIGURACION CORS START--------------------------------------------//
+
+//    //builder.Services.AddCors(options =>
+
+//    //options.AddPolicy("AllowWebApp", builder => builder
+//    //                                   .WithOrigins(new[] { "http://localhost:3000", "http://localhost:8080", "http://localhost:4200", "http://localhost:5173" })
+//    //                                   .AllowCredentials()
+//    //                                   .AllowAnyHeader()
+//    //                                   .AllowAnyMethod()
+//    //                                   ));
+
+//    var misReglasCors = "AllowWebApp";
+//    builder.Services.AddCors(opt =>
+//    {
+//        opt.AddPolicy(name: misReglasCors,
+//            builder => {
+//                builder.WithOrigins("http://localhost:5173")
+//                .AllowAnyHeader()
+//                .AllowAnyMethod();
+//                //builder.AllowAnyOrigin()
+//            });
+//    });
+
+//    //--------------------------------------------CONFIGURACION CORS END--------------------------------------------//
+
+
+//    builder.Services.AddControllers();
+//    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//    builder.Services.AddEndpointsApiExplorer();
+
+//    //----------------------------------------------CONFIGURACION SWAGGER START---------------------------------------------//
+//    builder.Services.AddSwaggerGen(c =>
+//    {
+//        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Login & Registration API", Version = "v1", Description = "API para manejo de HVI y RECAP con seguridad JWT" });
+
+//        //DEFINING SECURITY
+//        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+//        {
+//            In = ParameterLocation.Header,
+//            Description = "Insertar token",
+//            Name = "Authorization",
+//            Type = SecuritySchemeType.Http,
+//            BearerFormat = "JWT",
+//            Scheme = "bearer"
+//        });
+//        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+//    {
+//        {
+//            new OpenApiSecurityScheme
+//            {
+//                Reference = new OpenApiReference
+//                {
+//                    Type = ReferenceType.SecurityScheme,
+//                    Id = "Bearer"
+//                }
+//            },
+//            new string[] {}
+//        }
+//    });
+//    });
+//    //----------------------------------------------CONFIGURACION SWAGGER END---------------------------------------------//
+
+//    ////RGISTRAR AUTOMAPPER
+//    //builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//    //Depency injection for repository use
+//    builder.Services.AddScoped<IFileRepository, FileRepository>();
+//    builder.Services.AddScoped<IUserRepository, UserRepository>();
+//    builder.Services.AddScoped<ILoginRepository, LoginRepository>();
+//    builder.Services.AddScoped<JWTConfiguration>();
+//    builder.Services.AddScoped<IHviRepository, HviRepository>();
+
+//}
+
+
+
+//var app = builder.Build();
+//{
+//    // Configure the HTTP request pipeline.
+//    if (app.Environment.IsDevelopment())
+//    {
+//        app.UseSwagger();
+//        app.UseSwaggerUI();
+//    }
+
+//    //---------------------------------NECESARIO PARA CORS----------------------------------------//
+//    app.UseCors("AllowWebApp");
+//    //---------------------------------NECESARIO PARA CORS----------------------------------------//
+
+//    app.UseHttpsRedirection();
+
+//    //---------------------------------NECESARIO PARA JWT----------------------------------------//
+//    app.UseAuthentication();
+//    //---------------------------------NECESARIO PARA JWT----------------------------------------//
+
+//    app.UseAuthorization();
+
+//    app.MapControllers();
+
+//    app.Run();
+//}
+
+
 var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+
+//----------------------------------------------CONFIGURACION JWT START---------------------------------------------//
+
+builder.Configuration.AddJsonFile("appsettings.json");
+var secretKey = builder.Configuration.GetSection("settings").GetSection("secretkey").ToString();
+var keyBytes = Encoding.UTF8.GetBytes(secretKey);
+
+builder.Services.AddAuthentication(config =>
 {
-    // Add services to the container.
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 
-    //----------------------------------------------CONFIGURACION JWT START---------------------------------------------//
-
-    builder.Configuration.AddJsonFile("appsettings.json");
-    var secretKey = builder.Configuration.GetSection("settings").GetSection("secretkey").ToString();
-    var keyBytes = Encoding.UTF8.GetBytes(secretKey);
-
-    builder.Services.AddAuthentication(config =>
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = true;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
     {
-        config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
-    }).AddJwtBearer(config =>
-    {
-        config.RequireHttpsMetadata = true;
-        config.SaveToken = true;
-        config.TokenValidationParameters = new TokenValidationParameters
+//----------------------------------------------CONFIGURACION JWT END---------------------------------------------//
+
+//--------------------------------------------CONFIGURACION CORS START--------------------------------------------//
+
+//builder.Services.AddCors(options =>
+
+//options.AddPolicy("AllowWebApp", builder => builder
+//                                   .WithOrigins(new[] { "http://localhost:3000", "http://localhost:8080", "http://localhost:4200", "http://localhost:5173" })
+//                                   .AllowCredentials()
+//                                   .AllowAnyHeader()
+//                                   .AllowAnyMethod()
+//                                   ));
+
+var misReglasCors = "AllowWebApp";
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: misReglasCors,
+        builder =>
         {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
-
-    //----------------------------------------------CONFIGURACION JWT END---------------------------------------------//
-
-    //--------------------------------------------CONFIGURACION CORS START--------------------------------------------//
-
-    //builder.Services.AddCors(options =>
-
-    //options.AddPolicy("AllowWebApp", builder => builder
-    //                                   .WithOrigins(new[] { "http://localhost:3000", "http://localhost:8080", "http://localhost:4200", "http://localhost:5173" })
-    //                                   .AllowCredentials()
-    //                                   .AllowAnyHeader()
-    //                                   .AllowAnyMethod()
-    //                                   ));
-
-    var misReglasCors = "AllowWebApp";
-    builder.Services.AddCors(opt =>
-    {
-        opt.AddPolicy(name: misReglasCors,
-            builder => {
-                builder.WithOrigins("http://localhost:5173")
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-                //builder.AllowAnyOrigin()
-            });
-    });
-
-    //--------------------------------------------CONFIGURACION CORS END--------------------------------------------//
-
-
-    builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-
-    //----------------------------------------------CONFIGURACION SWAGGER START---------------------------------------------//
-    builder.Services.AddSwaggerGen(c =>
-    {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Login & Registration API", Version = "v1", Description = "API para manejo de HVI y RECAP con seguridad JWT" });
-
-        //DEFINING SECURITY
-        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        {
-            In = ParameterLocation.Header,
-            Description = "Insertar token",
-            Name = "Authorization",
-            Type = SecuritySchemeType.Http,
-            BearerFormat = "JWT",
-            Scheme = "bearer"
+            builder.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+            //builder.AllowAnyOrigin()
         });
-        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+});
+
+//--------------------------------------------CONFIGURACION CORS END--------------------------------------------//
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+//----------------------------------------------CONFIGURACION SWAGGER START---------------------------------------------//
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Login & Registration API", Version = "v1", Description = "API para manejo de HVI y RECAP con seguridad JWT" });
+
+    //DEFINING SECURITY
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
+        In = ParameterLocation.Header,
+        Description = "Insertar token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+{
         {
             new OpenApiSecurityScheme
             {
@@ -94,48 +227,40 @@ var builder = WebApplication.CreateBuilder(args);
             },
             new string[] {}
         }
-    });
-    });
-    //----------------------------------------------CONFIGURACION SWAGGER END---------------------------------------------//
+});
+});
+//----------------------------------------------CONFIGURACION SWAGGER END---------------------------------------------//
 
-    ////RGISTRAR AUTOMAPPER
-    //builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+////RGISTRAR AUTOMAPPER
+//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-    //Depency injection for repository use
-    builder.Services.AddScoped<IFileRepository, FileRepository>();
-    builder.Services.AddScoped<IUserRepository, UserRepository>();
-    builder.Services.AddScoped<ILoginRepository, LoginRepository>();
-    builder.Services.AddScoped<JWTConfiguration>();
-    builder.Services.AddScoped<IHviRepository, HviRepository>();
-
-}
-
-
+//Depency injection for repository use
+builder.Services.AddScoped<IFileRepository, FileRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ILoginRepository, LoginRepository>();
+builder.Services.AddScoped<JWTConfiguration>();
+builder.Services.AddScoped<IHviRepository, HviRepository>();
 
 var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    //---------------------------------NECESARIO PARA CORS----------------------------------------//
-    app.UseCors("AllowWebApp");
-    //---------------------------------NECESARIO PARA CORS----------------------------------------//
-
-    app.UseHttpsRedirection();
-
-    //---------------------------------NECESARIO PARA JWT----------------------------------------//
-    app.UseAuthentication();
-    //---------------------------------NECESARIO PARA JWT----------------------------------------//
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.Run();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+//---------------------------------NECESARIO PARA CORS----------------------------------------//
+app.UseCors("AllowWebApp");
+//---------------------------------NECESARIO PARA CORS----------------------------------------//
 
+app.UseHttpsRedirection();
+
+//---------------------------------NECESARIO PARA JWT----------------------------------------//
+app.UseAuthentication();
+//---------------------------------NECESARIO PARA JWT----------------------------------------//
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
